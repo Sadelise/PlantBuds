f5128eb5dbeb29ad45a2753626bec4f6aad3250a
sade <sharhio@hotmail.com>
2017-03-15 10:54:03 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="sharhio"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="PlantBuds"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
