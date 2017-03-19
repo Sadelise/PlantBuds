327774b7188fb2bb937865e3212939ff0c595026
Sadelise <sharhio@hotmail.com>
2017-03-19 17:44:58 +0200

    Sivujen suunnitelmat

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,8 +4,8 @@ class HelloWorldController extends BaseController {
 
     public static function index() {
         // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-//   	  View::make('home.html');
-        echo 'Tämä on etusivu!';
+   	  View::make('/suunnitelmat/frontpage.html');
+//        echo 'Tämä on etusivu!';
     }
 
     public static function sandbox() {
@@ -14,4 +14,34 @@ class HelloWorldController extends BaseController {
         View::make('helloworld.html');
     }
 
+    public static function edit_ownplant() {
+        View::make('suunnitelmat/edit_ownplant.html');
+    }
+
+    public static function edit_plant() {
+        View::make('suunnitelmat/edit_plant.html');
+    }
+
+    public static function frontpage() {
+        View::make('suunnitelmat/frontpage.html');
+    }
+
+    public static function list_ownplant() {
+        View::make('suunnitelmat/list_ownplant.html');
+    }
+
+    public static function list_plant() {
+        View::make('suunnitelmat/list_plant.html');
+    }
+
+    public static function plantdescription() {
+        View::make('suunnitelmat/plantdescription.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+    public static function caredescription() {
+        View::make('suunnitelmat/caredescription.html');
+    }
 }
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,41 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+
+$routes->get('/description', function() {
+    HelloWorldController::plantdescription();
+});
+
+$routes->get('/list_p', function() {
+    HelloWorldController::list_plant();
+});
+
+$routes->get('/list_o', function() {
+    HelloWorldController::list_ownplant();
+});
+
+$routes->get('/frontpage', function() {
+    HelloWorldController::frontpage();
+});
+
+$routes->get('/edit_p', function() {
+    HelloWorldController::edit_plant();
+});
+
+$routes->get('/edit_o', function() {
+    HelloWorldController::edit_ownplant();
+});
+
+$routes->get('/care', function() {
+    HelloWorldController::caredescription();
+});
