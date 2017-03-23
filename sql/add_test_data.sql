e117a4065f3a387dc53d5cb010d2b48d26e46687
Sadelise <sharhio@hotmail.com>
2017-03-23 14:07:30 +0200

    Tietokantataulut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,26 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Grower (username, password) VALUES ('Tiina', '1234');
+INSERT INTO Grower (username, password) VALUES ('Pertti', '5678');
+
+INSERT INTO Plant (tradename, latin_name) VALUES ('Jukkapalmu', 'Yucca elephantipes');
+INSERT INTO Plant (tradename, latin_name) VALUES ('Liekkipuu', 'Delonix regia');
+INSERT INTO Plant (tradename, latin_name) VALUES ('Limoviikuna', 'Ficus benjamina');
+
+CREATE VIEW Jukan_id AS (SELECT id FROM Plant Where tradename = 'Jukkapalmu' LIMIT 1);
+CREATE VIEW Kasvattajan_id AS (SELECT id FROM Grower Where username = 'Tiina' LIMIT 1);
+CREATE VIEW Pertin_id AS (SELECT id FROM Grower Where username = 'Pertti' LIMIT 1);
+
+INSERT INTO Plant_Description (plant_id, light, water, description, edited) VALUES ((SELECT * FROM Jukan_id) ,'Paljon', 'vähäinen', 'Paras kasvi', NOW());
+
+CREATE VIEW D_id AS (SELECT id FROM Plant_Description WHERE plant_id = (SELECT * FROM Jukan_id));
+
+INSERT INTO Writer (grower_id, description_id) VALUES ((SELECT * FROM Kasvattajan_id), (SELECT * FROM D_id));
+INSERT INTO Writer (grower_id, description_id) VALUES ((SELECT * FROM Pertin_id), (SELECT * FROM D_id));
+
+INSERT INTO Owned_Plant (grower_id, plant_id, acquisition, status, location, distance_window, soil, soil_description, watering, fertilizing, details, added) VALUES ((SELECT * FROM Kasvattajan_id), (SELECT * FROM Jukan_id), '2003-03-21', 'elossa', 'Pohjois-ikkuna', 0.5, 'kaupallinen', 'kekkilä', 'kerran viikossa', 'kerran kuukaudessa', 'Pistokkaasta kasvatettu', NOW() );
+INSERT INTO Owned_Plant (grower_id, plant_id, acquisition, status, location, distance_window, soil, soil_description, watering, fertilizing, details, added) VALUES ((SELECT * FROM Pertin_id), (SELECT * FROM Jukan_id), '2003-03-21', 'elossa', 'Etelä-ikkuna', 1.0, 'kaupallinen', 'biolan', 'kerran kuukaudessa', 'ei koskaan', 'Plantagenista ostettu', NOW() );
+INSERT INTO Owned_Plant (grower_id, plant_id, acquisition, status, location, distance_window, soil, soil_description, watering, fertilizing, details, added) VALUES ((SELECT * FROM Pertin_id), 2, '2003-03-21', 'elossa', 'Etelä-ikkuna', 0.5, 'sekoitus', 'Kookoskuitu+perliitti', 'kerran viikossa', 'kerran kahdessa viikossa', 'Siemenestä kasvatettu', NOW() );
+INSERT INTO Owned_Plant (grower_id, plant_id, acquisition, status, location, distance_window, soil, soil_description, watering, fertilizing, details, added) VALUES ((SELECT * FROM Pertin_id), 3, '05.12.2007', 'elossa', 'Etelä-ikkuna', 1.0, 'kaupallinen', 'kekkilä', 'kerran kuukaudessa', 'kerran kahdessa viikossa', 'Raision kukkatalosta ostettu.', NOW() );
+
+CREATE VIEW OP_id AS (SELECT id FROM Owned_Plant WHERE plant_id = (SELECT * FROM Jukan_id) LIMIT 1);
+
+INSERT INTO Diary (grower_id, owned_id, posted, post) VALUES ((SELECT * FROM Kasvattajan_id),(SELECT * FROM OP_id), NOW(), 'Kasvi kasvanut sentin eilisestä.');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,50 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Grower(
+  id SERIAL PRIMARY KEY, 
+  username varchar(50) NOT NULL, 
+  password varchar(50) NOT NULL
+);
+
+CREATE TABLE Plant(
+  id SERIAL PRIMARY KEY,
+  tradename varchar NOT NULL,
+  latin_name varchar
+);
+
+CREATE TABLE Plant_Description(
+  id SERIAL PRIMARY KEY,
+  plant_id INTEGER REFERENCES Plant(id),
+  light varchar,
+  water varchar,
+  description varchar,
+  edited DATE
+);
+
+CREATE TABLE Writer(
+  grower_id INTEGER REFERENCES Grower(id),
+  description_id INTEGER REFERENCES Plant_Description(id)
+);
+
+CREATE TABLE Owned_Plant(
+  id SERIAL PRIMARY KEY,
+  grower_id INTEGER REFERENCES Grower(id), 
+  plant_id INTEGER REFERENCES Plant(id),
+  acquisition date,
+  status varchar NOT NULL,
+  location varchar NOT NULL,
+  distance_window decimal NOT NULL,
+  soil varchar NOT NULL,
+  soil_description varchar NOT NULL,
+  watering varchar NOT NULL,
+  fertilizing varchar NOT NULL,
+  details varchar NOT NULL,
+  added DATE NOT NULL
+);
+
+CREATE TABLE Diary(
+  id SERIAL PRIMARY KEY,
+  grower_id INTEGER REFERENCES Grower(id),
+  owned_id INTEGER REFERENCES Owned_Plant(id),
+  posted timestamp NOT NULL,
+  post text NOT NULL 
+);
+
